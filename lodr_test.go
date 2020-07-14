02cb424c6c4cba508d28e579645f40cd87d553cd
Adel Abdelhak <adel.abdelhak.lille@gmail.com>
2020-07-14 23:32:06 +0200

    Add test cases

--- lodr_test.go
+++ lodr_test.go
@@ -1,34 +1,100 @@
 package lodr
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 )
 
-// TestConfig structure
-type TestConfig struct {
-	Name  string  `yaml:"yamlName" json:"jsonName" cmd:"app.name" env:"NAME"`
-	Float float64 `json:"jsonFloat" cmd:"app.thefloat"`
-	DB    struct {
-		Host    string        `yaml:"yamlHost" json:"jsonHost" cmd:"app.db.host"`
-		Port    int           `yaml:"yamlPort" json:"jsonPort" cmd:"app.db.port"`
-		Options []string      `yaml:"yamlOptions" json:"jsonOptions" cmd:"app.db.options"`
-		TLS     bool          `yaml:"yamlTLS" json:"jsonTLS" cmd:"tls" env:"IS_TLS"`
-		Timeout time.Duration `yaml:"yamlTimeout" json:"jsonTimeout" cmd:"timeout"`
+func TestJsonFile(t *testing.T) {
+	generateTestFiles(t)
+	defer deleteTestFiles(t)
+
+	type testConfig struct {
+		StringParam string        `json:"jsonStringParam"`
+		IntParam    int           `json:"jsonIntParam"`
+		FloatParam  float64       `json:"jsonFloatParam"`
+		TimeParam   time.Duration `json:"jsonTimeParam"`
+		BoolParam   bool          `json:"jsonBoolParam"`
 	}
-	Log struct {
-		Formats []struct {
-			Name   string `yaml:"yamlLogFormatName" json:"jsonLogFormatName"`
-			Pretty bool
-		}
+	var tc testConfig
+
+	Load(&tc).File("config.json")
+
+	if tc.StringParam != "json_string_param" {
+		t.Errorf("Should be json_string_param, is %s", tc.StringParam)
+	}
+	if tc.IntParam != 3 {
+		t.Errorf("Should be 3, is %d", tc.IntParam)
+	}
+	if tc.FloatParam != 3.1415 {
+		t.Errorf("Should be 3.1415, is %f", tc.FloatParam)
+	}
+	if tc.TimeParam != time.Second*15 {
+		t.Errorf("Should be 15s, is %s", tc.TimeParam)
+	}
+	if tc.BoolParam != true {
+		t.Errorf("Should be true, is %t", tc.BoolParam)
 	}
 }
 
-func TestLoad(t *testing.T) {
-	var tc TestConfig
-	tc.Name = "test"
-	config := Load(tc)
-	if config.object == nil {
-		t.Error("config.object should be 'test'")
+func TestEnv(t *testing.T) {
+	type testConfig struct {
+		StringParam string        `env:"ENV_STRING_PARAM"`
+		IntParam    int           `env:"ENV_INT_PARAM"`
+		FloatParam  float64       `env:"ENV_FLOAT_PARAM"`
+		TimeParam   time.Duration `env:"ENV_TIME_PARAM"`
+		BoolParam   bool          `env:"ENV_BOOL_PARAM"`
+	}
+	var tc testConfig
+
+	os.Setenv("ENV_STRING_PARAM", "env_string_param")
+	os.Setenv("ENV_INT_PARAM", "4")
+	os.Setenv("ENV_FLOAT_PARAM", "4.1234")
+	os.Setenv("ENV_TIME_PARAM", "10s")
+	os.Setenv("ENV_BOOL_PARAM", "true")
+
+	Load(&tc).Env()
+	if tc.StringParam != "env_string_param" {
+		t.Errorf("Should be env_string_param, is %s", tc.StringParam)
+	}
+	if tc.IntParam != 4 {
+		t.Errorf("Should be 4, is %d", tc.IntParam)
+	}
+	if tc.FloatParam != 4.1234 {
+		t.Errorf("Should be 4.1234, is %f", tc.FloatParam)
+	}
+	if tc.TimeParam != time.Second*10 {
+		t.Errorf("Should be 10s, is %s", tc.TimeParam)
+	}
+	if tc.BoolParam != true {
+		t.Errorf("Should be true, is %t", tc.BoolParam)
+	}
+}
+
+func generateTestFiles(t *testing.T) {
+	configJSON := []byte(`{
+	"jsonStringParam": "json_string_param",
+	"jsonIntParam": 3,
+	"jsonFloatParam": 3.1415,
+	"jsonTimeParam": 15000000000,
+	"jsonBoolParam": true
+}`)
+	err := ioutil.WriteFile("config.json", configJSON, 0644)
+	if err != nil {
+		t.Error("Could not generate test file config.json")
+	}
+}
+
+func deleteTestFiles(t *testing.T) {
+	files := []string{
+		"config.json",
+	}
+	for _, file := range files {
+		err := os.Remove(file)
+		if err != nil {
+			t.Errorf("Could not delete test file %s", file)
+		}
 	}
 }
